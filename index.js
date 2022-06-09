f17de9600202bbb7cdf117b58522a46d6a1a3694
hasanozacar <hsnozacar@gmail.com>
2022-06-09 13:54:09 +0300

    Add rental and Movies

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const Joi = require('joi');
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const express = require('express');
-const mongoose = require("mongoose");
 const app = express();
 
-mongoose
-  .connect("mongodb://localhost/customers")
-  .then(() => console.log("Connected MongoDb"))
-  .catch((err) => console.log("Could not connect to mongodb", err));
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
