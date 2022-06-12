9ccb2fe28d0f75016ce1ea9f0690ce9f980ca8e1
hasanozacar <hsnozacar@gmail.com>
2022-06-13 00:28:19 +0300

    Add Auth

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const user = require('./routes/user');
+const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 
@@ -17,6 +18,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/user', user);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
