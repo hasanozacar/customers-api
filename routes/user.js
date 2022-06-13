e1b2dad583d9bae0fdcff4b343d4ad5eee857e25
hasanozacar <hsnozacar@gmail.com>
2022-06-13 16:37:02 +0300

    Protecting Routes

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,15 @@ const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
-const config = require('config');
-const jwt = require('jsonwebtoken');
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
+ 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -25,8 +31,10 @@ router.post("/", async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
-    const token = user.generateAuthToken();    
-    res.header('x-auth-token',token).send(_.pick(user, ["_id", "name", "email"]));
+    const token = user.generateAuthToken();
+    res
+      .header("x-auth-token", token)
+      .send(_.pick(user, ["_id", "name", "email"]));
   } catch (error) {
     console.log(error);
   }
